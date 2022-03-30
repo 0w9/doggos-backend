38cb0d159883e93034f680e66f1e077951fac193
0w9 <79471472+0w9@users.noreply.github.com>
2022-03-30 22:42:44 +0200

    Fix.

--- helpers/editJson.js
+++ helpers/editJson.js
@@ -9,20 +9,20 @@ async function editJson(target_trait, new_value, file_id) {
         try {
             const docData = doc.data();
             console.log(docData.attributes);
-            if(target_trait in await docData.attributes) {
 
-                const index = await docData.attributes.findIndex(element => element.trait_type === target_trait);
+            const index = await docData.attributes.findIndex(element => element.trait_type === target_trait);
+            const new_attributes = await docData.attributes;
+            new_attributes[index] = {trait_type: target_trait, value: new_value}
+
+            const res = await db.collection('files').doc(file_id).update({
+                attributes: new_attributes
+
+                });
+
+            console.log(index);
+
+            return true;
             
-                const new_attributes = await docData.attributes;
-                new_attributes[index] = {trait_type: target_trait, value: new_value}
-
-                const res = await db.collection('files').doc(file_id).update({
-                    attributes: new_attributes
-                 }); 
-
-                return true;
-            
-                } else { return false; }
         } catch(e) {
             return false;
         }
