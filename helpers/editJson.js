82516f72c091b38e7fd8b9d5fb36bd9612edcc2a
Lennard Dorst <79471472+0w9@users.noreply.github.com>
2022-04-04 21:51:46 +0200

    Finial.

--- helpers/editJson.js
+++ helpers/editJson.js
@@ -2,27 +2,23 @@
 const db = require("./initDB");
 
 async function editJson(target_trait, new_value, file_id) {
-    const doc = await db.collection('files').doc(file_id).get();
 
+    const doc = await db.doc("/files/CRGJ7yROqi0Ie9myjXHQ").get();
     if (doc.exists) {
 
-        try {
-            const docData = doc.data();
+        const docData = doc.data();
+        console.log(docData);
+        const index = await docData.attributes.findIndex(element => element.trait_type === target_trait);
+        const new_attributes = await docData.attributes;
+        new_attributes[index] = {trait_type: target_trait, value: new_value}
 
-            const index = await docData.attributes.findIndex(element => element.trait_type === target_trait);
-            const new_attributes = await docData.attributes;
-            new_attributes[index] = {trait_type: target_trait, value: new_value}
+        await db.collection('files').doc(file_id.replace(/\0[\s\S]*$/g,'')).update({
+            attributes: new_attributes});
 
-            await db.collection('files').doc(file_id).update({
-                attributes: new_attributes
+        return true;
 
-                });
-
-            return true;
-            
-        } catch(e) {
-            return false;
-        }
+    } else {
+        return "There is no document.";
 }}
 
 module.exports = editJson;
